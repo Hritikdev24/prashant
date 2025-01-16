0ebc53e847b99b4d43200443f5c58c61d04999cd
HritikGangadhar <hritikdev24@gmail.com>
2025-01-16 21:22:51 +0530

    new update are added

--- home.js
+++ home.js
@@ -9,13 +9,15 @@ function designation(a){
 function  time(){
 
     const time=new Date();
+    console.log(time);
     return time.toLocaleTimeString();
+   
 
 }
 
   export default  function day(){
     const time=new Date();
-     
+     console.log(time);
     const arr=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
       return arr[time.getDay()];
 }
