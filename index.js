7d020740a4c42a4bf6fa7cdca13ee617b5a0dbb3
HritikDev <hritikdev24@gmail.com>
2025-01-16 16:58:51 +0530

    validator added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-piper is there
+function validator(a){
+    if(a>18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+function gender(a){
+    if(a=="male"){
+        return "Mr"
+    }else{
+        return "Mis"
+    }
+}
+
+
+function   cal(a){
+         return a*3;
+}
